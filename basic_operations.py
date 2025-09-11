85e140780dcb1ebb4a7bd2f93afa6bbd1a8ad88f
Ishimwe Lambert <ishimwelambert87@gmail.com>
2025-09-11 19:39:59 +0200

    addition in python

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+sum=number1+number2
+difference=number1-number2
+product=number1*number2
+print(f"the sum of {number1} and {number2}is {sum}")
+print(f"the difference of {number1} and {number2}is {difference}")
+print(f"the product of {number1} and {number2}is {product}")
