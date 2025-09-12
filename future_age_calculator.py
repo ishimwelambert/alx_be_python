cf67f4d723f77f36ee4aa3f9c68c897309b7b933
Ishimwe Lambert <ishimwelambert87@gmail.com>
2025-09-12 14:35:49 +0200

    changes to age calculator

--- future_age_calculator.py
+++ future_age_calculator.py
@@ -1,5 +1,5 @@
 current_age=int(input("How old are you?"))
 current_year=2023
 future_year=2050
-future_age=(future_year-current_year)+current_age
-print(f"In {future_year},you will be {future_age} years old.")
+future_age_calculator=(future_year-current_year)+current_age
+print(f"In {future_year},you will be {future_age_calculator} years old.")
