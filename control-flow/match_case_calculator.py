d695f41980c9d4f3874610a6d70312c9dd16d3fd
Ishimwe Lambert <ishimwelambert87@gmail.com>
2025-09-20 18:38:26 +0200

    calculator using match

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
-operator=input(" Choose the operation (+, -, *, /):")
+operator=input("Choose the operation (+, -, *, /):")
 match operator:
     case addition if operator=="+":
         print(f"The result is {num1+num2}")
