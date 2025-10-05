81432f1b73a82a3dfcf01854f14076ccf1c6bcff
Ishimwe Lambert <ishimwelambert87@gmail.com>
2025-10-05 11:49:09 +0200

    changes to depost and withdraw

--- python_introduction/finance_calculator.py
+++
@@ -1,6 +0,0 @@
-monthly_income=int(input("Enter your monthly income:"))
-monthly_expenses=int(input("Enter your monthly expense:"))
-monthly_savings = monthly_income - monthly_expenses
-rate=0.05
-projected_saving=monthly_savings*12+(monthly_savings*12*0.05)
-print(f"projected savings after one year, with interest, is:{projected_saving}")
